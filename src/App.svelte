51a53b9377295b765ee4bcb4e21e387389eefe69
Mickey <kmin950@gmail.com>
2021-03-16 11:09:18 +0900

    Add movie api

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
     import Router, { location } from 'svelte-spa-router'
     import routes from '~/routes'
     import Header from '~/components/Header.svelte'  
-    import Footer from '~/components/Footer.svelte'  
+    import Footer from '~/components/Footer.svelte'     
 </script>
 
 <Header />
